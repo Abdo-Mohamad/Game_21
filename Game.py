40fefe1742a93736f3971084847d64b239b6c632
abdo-mohamad <abdomohamadwalid@gmail.com>
2022-12-15 20:42:16 +0100

    create fuction

--- Game.py
+++ Game.py
@@ -1,4 +1,20 @@
 import random
 
-listaCarta = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]*4
-print(listaCarta)
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]*4
+
+
+def deal(deck):
+    hand = []
+    for i in range(2):
+        random.shuffle(deck)
+        card = deck.pop()
+        if card == 11:card = "J"
+        if card == 12:card = "Q"
+        if card == 13:card = "K"
+        if card == 14:card = "A"
+        hand.append(card)
+    return hand
+    
+
+print(deal(deck))
+
