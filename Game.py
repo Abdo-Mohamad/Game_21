dd2b3485cc8e83992b337bbcb2c45b86e033dfec
Articunogem12 <adrifer1705@gmail.com>
2022-12-16 20:54:46 +0100

    interfaz basica juego

--- Game.py
+++ Game.py
@@ -2,19 +2,77 @@ import random
 
 deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]*4
 
-presentacion= input("")
-def deal(deck):
+option= input("Hola y bienvenido al juego del 21, selecciona s para jugar, r para ver las reglas o q para salir: ")
+
+
+def deal(deck,numCards):
     hand = []
-    for i in range(2):
+    if numCards== 2 :
         random.shuffle(deck)
-        card = deck.pop()
-        if card == 11:card = "J"
-        if card == 12:card = "Q"
-        if card == 13:card = "K"
-        if card == 14:card = "A"
-        hand.append(card)
-    return hand
-    
+        for i in range(numCards):
+                card = deck.pop()
+                if card == 11:card = "J"
+                if card == 12:card = "Q"
+                if card == 13:card = "K"
+                if card == 14:card = "A"
+                hand.append(card)
+        return hand
+    else:
+        for i in range(numCards):
+                    card = deck.pop()
+                    if card == 11:card = "J"
+                    if card == 12:card = "Q"
+                    if card == 13:card = "K"
+                    if card == 14:card = "A"
+                    hand.append(card)
+        return hand
+while option:
+    if option == "s":
 
-print(deal(deck))
+        
+        while True:
+                print("Has seleccionado jugar")
+                # time.sleep(1)
+                print("Mezclando baraja.")
+                # time.sleep(1)
+                print("Mezclando baraja..")
+                # time.sleep(1)
+                print("Mezclando baraja...")
+                # time.sleep(1)
+                print("Terminado")
+                player_hand = deal(deck,2)
+                print(player_hand)
+                
+                
 
+                # reserva = str(numero) + str(deal(deck))
+                # numero = 0
+                # # time.sleep(2)
+
+                seguir = input("Si quieres robar pulsa r, si te quieres rendir pulsa f: ")
+                if seguir == "r" :
+                    print("aqui estamos")
+                    test = deal(deck,1)
+                    print(player_hand)
+                    test.append(player_hand)
+                    print(test)
+
+                    break
+                elif seguir == "f":
+                    print("la consola ")
+                    break
+                else:
+                    print("exet")
+                exit()
+        break    
+    elif option == "r":
+
+            print("Estas son las reglas: ")
+            break
+    elif option == "q":
+            print("Hasta luego")
+            exit()
+
+    else:
+        option = input(
+            "Lo siento, las unicas opciones son s para jugar, r para ver las reglas o q para salir, selecciona 1: ")
